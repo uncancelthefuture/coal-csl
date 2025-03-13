ee27d2940a1f624d89136c0ac344b8b232b9c9a6
uncancelthefuture <wilsonpoulter@hotmail.com>
2025-03-13 19:40:06 -0400

    change folder strucutre

--- render.py
+++ render.py
@@ -1,36 +1,51 @@
 from bs4 import BeautifulSoup
 from glob import glob
+import re
+from datetime import datetime
+import pytz
+from lxml import etree
 
 def run():
-    csl = BeautifulSoup('<style xmlns="http://purl.org/net/xbiblio/csl" class="note" version="1.0.2"></style>', 'xml')
+    csl = BeautifulSoup('<style xmlns="http://purl.org/net/xbiblio/csl" class="note" version="1.0"></style>', 'xml')
 
     parts = glob('coal-rjal/*')
+    parts.sort()
 
     for part in parts:
-        part_type = part.replace('coal-rjal/','')
-        part_tag = BeautifulSoup(f'<{part_type}></{part_type}>', 'xml')
+        part_type = re.sub('coal-rjal/','', part)
+        part_name = re.sub('\d-','',part_type)
+        part_tag = BeautifulSoup(f'<{part_name}></{part_name}>', 'xml')
         i = 0
         for filename in glob(f'coal-rjal/{part_type}/**/*.xml', recursive=True):
             with open(filename, 'r') as f:
                 tag = BeautifulSoup(f.read(), "xml")
-                if part_type == 'macros':
+                if part_name == 'macros':
                     csl.style.append(tag)
+                elif part_name == 'locale':
+                    sub_tags = tag.find_all('term')
+                    for sub_tag in sub_tags:
+                        part_tag.find(part_name).insert(i, sub_tag)
                 else:
-                    part_tag.find(part_type).insert(i, tag)
+                    part_tag.find(part_name).insert(i, tag)
                 i += 1
-        if part_type != "macros":
+        if part_name != "macros":
             csl.style.append(part_tag)
     csl.style.locale['xml:lang'] = "en"
-    csl.style.citation["et-al-min"]="4" 
-    csl.style.citation["et-al-use"]="1"
     terms = csl.find_all('term')
     csl.locale.append(BeautifulSoup('<terms></terms>', 'xml'))
     for term in terms:
         csl.style.terms.append(term)
+    csl.info.append(BeautifulSoup(f'<updated>{datetime.now(pytz.utc).isoformat()}</updated>', 'xml'))
 
     with open('demo/coal-rjal.csl', 'w') as f:
-        f.write(str(csl).replace('</terms><terms>','').replace('<terms/>',''))
+        f.write(csl.prettify().replace('<terms></terms>',''))
         f.truncate()
+    tree = etree.parse('demo/coal-rjal.csl')
+    root = tree.getroot()
+    for elem in root.iter('*'):
+        if elem.text is not None:
+            elem.text = elem.text.strip()
+    tree.write('demo/coal-rjal.csl')
 
 if __name__ == "__main__":
     run()
